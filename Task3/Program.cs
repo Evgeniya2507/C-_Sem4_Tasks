2659becd7665d42a648b302b5f2a9607556a2d11
Evgeniya.S <reg.subbotina@gmail.com>
2023-04-05 23:48:04 +0300

    added Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+int [] numbers = new int[8];
+int length = numbers.Length;
+
+void Array(int length)
+{
+    for ( int i = 0; i < length; i++)
+    {
+        numbers[i] = new Random().Next(1, 100);
+        System.Console.Write(numbers[i] + " ");
+    }
+}
+
+Array(length);
